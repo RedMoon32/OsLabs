5396ee182f254e1dcada1b6a4e1cb541e368c644
Rinat <rinat.babichev@gmail.com>
2018-09-26 22:48:10 +0300

    Fixes

--- week6/ex5.c
+++ week6/ex5.c
@@ -2,6 +2,10 @@
 #include <unistd.h>
 #include <signal.h>
 
+void sigterm_handler(int sign){
+	printf("Child receveid sigterm signal\n");
+}
+
 int main()	
 {
 	int n=getpid();
@@ -11,6 +15,7 @@ int main()
 		kill(child,SIGTERM);
 	}
 	else{
+		signal(SIGTERM,sigterm_handler);
 		while(1){
 			printf("Still alive\n");
 			sleep(1);
